ec7d95ff1278500258a55529340d3919f0b9c9e4
XXXDESHXXX <kirya.shvarts@list.ru>
2023-09-15 11:49:24 +0300

    First solved task with two pointers init.

---
+++ Easy_tasks/Algorithms/Count Pairs Whose Sum is Less than Target.py
@@ -0,0 +1,29 @@
+# Variant without method sort()
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = 1
+        count = 0
+        while left < len(nums):
+            cur_sum = nums[left] + nums[right]
+            if cur_sum < target and left < right:
+                count += 1
+            if right == len(nums) - 1:
+                left += 1
+                right = 0
+            right += 1
+        return count
+# Variant with sort() method
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        left = 0
+        right = len(nums) - 1
+        cnt = 0
+        while left < right:
+            if nums[left] + nums[right] < target:
+                cnt += right - left
+                left += 1
+            else:
+                right -= 1
+        return cnt
