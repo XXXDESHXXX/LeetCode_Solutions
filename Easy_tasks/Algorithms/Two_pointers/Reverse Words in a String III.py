4ef904155d721774037f45c8e311cd856c5dfa19
XXXDESHXXX <kirya.shvarts@list.ru>
2023-09-18 12:37:24 +0300

    2 Solved tasks with two pointers algorithm

---
+++ Easy_tasks/Algorithms/Two_pointers/Reverse Words in a String III.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s_strip = s.split()
+        result = ''
+        for word in s_strip:
+            result += word[::-1] + ' '
+        return result.rstrip(' ')
