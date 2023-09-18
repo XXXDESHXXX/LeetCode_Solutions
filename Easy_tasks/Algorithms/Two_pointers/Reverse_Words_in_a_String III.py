c9a109c6d2b88413cb39a0c022d31e184c92f0f8
XXXDESHXXX <kirya.shvarts@list.ru>
2023-09-18 14:00:05 +0300

    Rename files

--- Easy_tasks/Algorithms/Two_pointers/Reverse_Words_in_a_String III.py
+++ Easy_tasks/Algorithms/Two_pointers/Reverse_Words_in_a_String III.py
@@ -1,3 +1,4 @@
+# First solution without two pointers algorithm:
 class Solution:
     def reverseWords(self, s: str) -> str:
         s_strip = s.split()
@@ -5,4 +6,5 @@ class Solution:
         for word in s_strip:
             result += word[::-1] + ' '
         return result.rstrip(' ')
+# Second solution with two pointers algorithm:
 
