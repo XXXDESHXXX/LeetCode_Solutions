6330a7fbeea1977bf83395258d416b783208c459
XXXDESHXXX <kirya.shvarts@list.ru>
2023-09-29 16:54:41 +0300

    BFS Task

---
+++ Easy_tasks/Algorithms/BFS/Same_Tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        queue = [(p,q)]
+        while queue:
+            node1, node2 = queue.pop(0)
+            if not node1 and not node2:
+                continue
+            elif not node1 or not node2:
+                return False
+            elif node1.val != node2.val:
+                return False
+            else:
+                queue.append((node1.left,node2.left))
+                queue.append((node1.right,node2.right))
+        return True
